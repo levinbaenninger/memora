629a2eb21e56f954a931f63c15e72f710482a4a1
Tobias Kuhn <tobias.kuhn22@icloud.com>
2026-05-28 10:30:34 +0200

    refactor: update tag schema imports to use note-tags module in links procedures

--- packages/api/src/router.ts
+++ packages/api/src/router.ts
@@ -2,8 +2,8 @@ import type { RouterClient } from "@orpc/server";
 
 import { foldersRouter } from "./modules/folders/router";
 import { linksRouter } from "./modules/links/router";
+import { tagsRouter } from "./modules/note-tags/router";
 import { notesRouter } from "./modules/notes/router";
-import { tagsRouter } from "./modules/tags/router";
 
 export const appRouter = {
   notes: {
